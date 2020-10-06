81bb6b9eff5df8858689b2f967938b89c3da8888
irubenboy <rubenmoreno.saladillo@gmail.com>
2020-10-06 11:16:24 +0200

    Corrección
    Se ha corregido el ejercicio de las potencias

--- src/classes/Powers.java
+++ src/classes/Powers.java
@@ -11,7 +11,7 @@ public class Powers extends Thread{
     @Override
     public void run(){
         for(int i = 1; i <= 10; i++){
-            System.out.printf("%s: %d ^ %d = %d\n", getName(), number, i, i * number);
+            System.out.printf("%s: %d ^ %d = %d\n", getName(), number, i, Math.pow(number, i));
         }
     }
 
